fb3a349d85103c2b09610e92eb86668b77f97d4a
CoderElectronics <aristehney@gmail.com>
2026-08-01 18:16:02 -0700

    updating function names

--- lib/mirror_bindings.h
+++ lib/mirror_bindings.h
@@ -15,15 +15,15 @@
  * these will be compiled into the app binary and used to transfer data to the host via syscalls
  */
 
-static inline void mirror_putc(char c) {
+static inline void putc(char c) {
     syscall((uint32_t)SYS_PUTC, (uint32_t)c, 0);
 }
 
-static inline void mirror_println(const char *str) {
+static inline void println(const char *str) {
     syscall((uint32_t)SYS_PRINTLN, (uint32_t)str, 0);
 }
 
-static inline void mirror_lcd_rotate_point(int16_t *x0, int16_t *y0, int16_t angleDeg) {
+static inline void lcd_rotate_point(int16_t *x0, int16_t *y0, int16_t angleDeg) {
     static ffi_lcd_rotate_pt_cmd_t cmd;
     cmd.x0 = *x0;
     cmd.y0 = *y0;
@@ -33,123 +33,123 @@ static inline void mirror_lcd_rotate_point(int16_t *x0, int16_t *y0, int16_t ang
     *y0 = cmd.y0;
 }
 
-static inline void mirror_lcd_draw_fast_line(int x0, int y0, int x1, int y1, int color) {
+static inline void lcd_draw_fast_line(int x0, int y0, int x1, int y1, int color) {
     static ffi_lcd_line_cmd_t cmd;
     cmd.x0 = x0; cmd.y0 = y0; cmd.x1 = x1; cmd.y1 = y1; cmd.color = color;
     syscall((uint32_t)SYS_LCD_DRAW_FAST_LINE, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_rect(int x, int y, int w, int h, int color) {
+static inline void lcd_draw_rect(int x, int y, int w, int h, int color) {
     static ffi_lcd_rect_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.w = w; cmd.h = h; cmd.color = color;
     syscall((uint32_t)SYS_LCD_DRAW_RECT, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_fill_rect(int x, int y, int w, int h, int color) {
+static inline void lcd_fill_rect(int x, int y, int w, int h, int color) {
     static ffi_lcd_rect_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.w = w; cmd.h = h; cmd.color = color;
     syscall((uint32_t)SYS_LCD_FILL_RECT, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_circle(int x0, int y0, int r, int color) {
+static inline void lcd_draw_circle(int x0, int y0, int r, int color) {
     static ffi_lcd_circle_cmd_t cmd;
     cmd.x = x0; cmd.y = y0; cmd.r = r; cmd.color = color;
     syscall((uint32_t)SYS_LCD_DRAW_CIRCLE, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_fill_circle(int x0, int y0, int r, int color) {
+static inline void lcd_fill_circle(int x0, int y0, int r, int color) {
     static ffi_lcd_circle_cmd_t cmd;
     cmd.x = x0; cmd.y = y0; cmd.r = r; cmd.color = color;
     syscall((uint32_t)SYS_LCD_FILL_CIRCLE, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_round_rect(int x, int y, int w, int h, int r, int color) {
+static inline void lcd_draw_round_rect(int x, int y, int w, int h, int r, int color) {
     static ffi_lcd_round_rect_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.w = w; cmd.h = h; cmd.r = r; cmd.color = color;
     syscall((uint32_t)SYS_LCD_DRAW_ROUND_RECT, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_fill_round_rect(int x, int y, int w, int h, int r, int color) {
+static inline void lcd_fill_round_rect(int x, int y, int w, int h, int r, int color) {
     static ffi_lcd_round_rect_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.w = w; cmd.h = h; cmd.r = r; cmd.color = color;
     syscall((uint32_t)SYS_LCD_FILL_ROUND_RECT, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_fill_rect_pattern(int x, int y, int w, int h, const uint8_t *pattern) {
+static inline void lcd_fill_rect_pattern(int x, int y, int w, int h, const uint8_t *pattern) {
     static ffi_lcd_pattern_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.w = w; cmd.h = h;
     syscall((uint32_t)SYS_LCD_FILL_RECT_PATTERN, (uint32_t)&cmd, (uint32_t)pattern);
 }
 
-static inline void mirror_lcd_draw_rect_rotated(int cenX, int cenY, int w, int h, int angleDeg, int color) {
+static inline void lcd_draw_rect_rotated(int cenX, int cenY, int w, int h, int angleDeg, int color) {
     static ffi_lcd_rot_rect_cmd_t cmd;
     cmd.cx = cenX; cmd.cy = cenY; cmd.w = w; cmd.h = h; cmd.angle = angleDeg; cmd.color = color;
     syscall((uint32_t)SYS_LCD_DRAW_RECT_ROTATED, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_char(int x, int y, unsigned char c, const void *font, int color, uint8_t size) {
+static inline void lcd_draw_char(int x, int y, unsigned char c, const void *font, int color, uint8_t size) {
     static ffi_lcd_char_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.c = c; cmd.font_ptr = (uint32_t)font; cmd.color = color; cmd.size = size;
     syscall((uint32_t)SYS_LCD_DRAW_CHAR, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_char_lc(int x, int y, unsigned char c, const void *font, int color, uint8_t size) {
+static inline void lcd_draw_char_lc(int x, int y, unsigned char c, const void *font, int color, uint8_t size) {
     static ffi_lcd_char_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.c = c; cmd.font_ptr = (uint32_t)font; cmd.color = color; cmd.size = size;
     syscall((uint32_t)SYS_LCD_DRAW_CHAR_LC, (uint32_t)&cmd, 0);
 }
 
-static inline void mirror_lcd_draw_font(int x, int y, const char *str, const void *font, int color, uint8_t size) {
+static inline void lcd_draw_font(int x, int y, const char *str, const void *font, int color, uint8_t size) {
     static ffi_lcd_font_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.font_ptr = (uint32_t)font; cmd.color = color; cmd.size = size;
     syscall((uint32_t)SYS_LCD_DRAW_FONT, (uint32_t)&cmd, (uint32_t)str); // Pass str via ARG1 for bounds checking
 }
 
-static inline void mirror_lcd_draw_font_lc(int x, int y, const char *str, const void *font, int color, uint8_t size) {
+static inline void lcd_draw_font_lc(int x, int y, const char *str, const void *font, int color, uint8_t size) {
     static ffi_lcd_font_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.font_ptr = (uint32_t)font; cmd.color = color; cmd.size = size;
     syscall((uint32_t)SYS_LCD_DRAW_FONT_LC, (uint32_t)&cmd, (uint32_t)str);
 }
 
-static inline void mirror_lcd_get_font_bounds(const char *str, const void *font, int x, int y, uint8_t size, int *x1, int *y1, int *w, int *h) {
+static inline void lcd_get_font_bounds(const char *str, const void *font, int x, int y, uint8_t size, int *x1, int *y1, int *w, int *h) {
     static ffi_lcd_font_bounds_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.font_ptr = (uint32_t)font; cmd.size = size;
     syscall((uint32_t)SYS_LCD_GET_FONT_BOUNDS, (uint32_t)&cmd, (uint32_t)str);
     *x1 = cmd.x1; *y1 = cmd.y1; *w = cmd.w; *h = cmd.h; // Read populated bounding box values
 }
 
-static inline void mirror_lcd_get_font_bounds_lc(const char *str, const void *font, int x, int y, uint8_t size, int *x1, int *y1, int *w, int *h) {
+static inline void lcd_get_font_bounds_lc(const char *str, const void *font, int x, int y, uint8_t size, int *x1, int *y1, int *w, int *h) {
     static ffi_lcd_font_bounds_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.font_ptr = (uint32_t)font; cmd.size = size;
     syscall((uint32_t)SYS_LCD_GET_FONT_BOUNDS_LC, (uint32_t)&cmd, (uint32_t)str);
     *x1 = cmd.x1; *y1 = cmd.y1; *w = cmd.w; *h = cmd.h;
 }
 
-static inline const void* mirror_lcd_get_font_mirror_icons12(void) {
+static inline const void* lcd_get_font_mirror_icons12(void) {
     static ffi_lcd_get_font_cmd_t cmd;
     syscall((uint32_t)SYS_LCD_GET_FONT_MIRROR_ICONS12, (uint32_t)&cmd, 0);
     return (const void*)cmd.font_ptr;
 }
 
-static inline const void* mirror_lcd_get_font_org_01(void) {
+static inline const void* lcd_get_font_org_01(void) {
     static ffi_lcd_get_font_cmd_t cmd;
     syscall((uint32_t)SYS_LCD_GET_FONT_ORG_01, (uint32_t)&cmd, 0);
     return (const void*)cmd.font_ptr;
 }
 
-static inline void mirror_msleep(int32_t ms) {
+static inline void msleep(int32_t ms) {
     syscall((uint32_t)SYS_MSLEEP, (uint32_t)ms, 0);
 }
 
-static inline void mirror_lcd_update(void) {
+static inline void lcd_update(void) {
     syscall((uint32_t)SYS_LCD_UPDATE, 0, 0);
 }
 
-static inline void mirror_lcd_clear(int color) {
+static inline void lcd_clear(int color) {
     syscall((uint32_t)SYS_LCD_CLEAR, (uint32_t)color, 0);
 }
 
-static inline void mirror_lcd_set_pixel(int x, int y, int color) {
+static inline void lcd_set_pixel(int x, int y, int color) {
     static ffi_lcd_set_pixel_cmd_t cmd;
     cmd.x = x; cmd.y = y; cmd.color = color;
     syscall((uint32_t)SYS_LCD_SET_PIXEL, (uint32_t)&cmd, 0);
